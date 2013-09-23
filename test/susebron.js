e57bb174012a7cafb0b942d88dcababad11ff972
Jon Packer <contact@jonpacker.com>
2013-09-23 15:14:02 +0200

    Added more tests that test things

--- test/susebron.js
+++ test/susebron.js
@@ -11,4 +11,73 @@ describe('susebron', function() {
     assert(!res.match(/\$potato/));
     done();
   });
+  it('should read config from a file', function(done) {
+    var stystr = 'body { font-family: $potato }';
+    var res = stylus(stystr)
+                .use(su('./test/test.json'))
+                .render();
+    assert(res.match(/Amazing/));
+    assert(!res.match(/\$potato/));
+    done();
+  });
+  it('should set a referential variable', function(done) {
+    var stystr = 'body { font-family: $potato }';
+    var res = stylus(stystr)
+                .use(su({ defs: {
+                  '$superthing': 'Amazing',
+                  '$potato': '$superthing'
+                } }))
+                .render();
+    assert(res.match(/Amazing/));
+    assert(!res.match(/\$potato/));
+    assert(!res.match(/\$superthing/));
+    done();
+  });
+  it('variable order should not matter', function(done) {
+    var stystr = 'body { font-family: $potato }';
+    var res = stylus(stystr)
+                .use(su({ defs: {
+                  '$potato': '$superthing',
+                  '$superthing': 'Amazing'
+                } }))
+                .render();
+    assert(res.match(/Amazing/));
+    assert(!res.match(/\$potato/));
+    assert(!res.match(/\$superthing/));
+    done();
+  });
+  it('should follow complex dependencies', function(done) {
+    var stystr = 'body { font-family: $potato }';
+    var res = stylus(stystr)
+                .use(su({ defs: {
+                  '$yep': 'Amazing',
+                  '$omg': '$really',
+                  '$potato': '$superthing',
+                  '$really': '$surelynot',
+                  '$superthing': '$omg',
+                  '$surelynot': '$yep'
+                } }))
+                .render();
+    assert(res.match(/Amazing/));
+    assert(!res.match(/\$potato/));
+    done();
+  });
+  it('should utilize existing globals', function(done) {
+    var stystr = 'body { font-family: $potato }';
+    var res = stylus(stystr)
+                .use(su({
+                  defs: { '$yep': 'Amazing' }
+                }))
+                .use(su({ defs: {
+                  '$omg': '$really',
+                  '$potato': '$superthing',
+                  '$really': '$surelynot',
+                  '$superthing': '$omg',
+                  '$surelynot': '$yep'
+                } }))
+                .render();
+    assert(res.match(/Amazing/));
+    assert(!res.match(/\$potato/));
+    done();
+  });
 });
